062a4a83ae4cc03d7a9ac7a48457526d7a11f898
binz1215 <binz1215@naver.com>
2023-12-13 16:02:21 +0900

    module 수정

--- src/view/inputView.js
+++ src/view/inputView.js
@@ -1,8 +1,9 @@
-import { Console } from '@woowacourse/mission-utils';
+import { MissionUtils } from '@woowacourse/mission-utils';
 
-async () {
-  const purchase = await Console.readLineAsync('\n');
+function getCoachName() {
+  const coach = MissionUtils.Console.readLine('코치의 이름을 입력해 주세요. (, 로 구분)\n');
+
+  return coach;
 }
 
-
-
+getCoachName();
